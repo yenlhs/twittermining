130f40de6482ca412c2b59e7f2006a848927db00
Adrian <yenlhs@gmail.com>
2018-10-02 22:58:13 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+set the following environment variable before running the scripts
+TWITTER_CONSUMER_KEY
+TWITTER_CONSUMER_SECRET
+TWITTER_ACCESS_TOKEN
+TWITTER_ACCESS_SECRET
---
+++ twitter_get_home_timeline.py
@@ -0,0 +1,11 @@
+import json 
+from tweepy import Cursor 
+from twitter_client import get_twitter_client 
+ 
+if __name__ == '__main__': 
+  client = get_twitter_client() 
+ 
+  with open('home_timeline.json', 'w') as f: 
+    for page in Cursor(client.home_timeline, count=200).pages(4): 
+      for status in page: 
+        f.write(json.dumps(status._json)+"\n") 
